31e1b8daee1dced83579b03a0d78bbb6b888b094
Icarus <520icarus1314@gmail.com>
2022-01-17 10:49:00 +0800

    feat: insertion-sort
    增加了插入排序(升/降序)

--- sort/insertion.go
+++ sort/insertion.go
@@ -1,149 +1,15 @@
+// Package sort
+// @Author Icarus-0727
+// @QQ 1067177968
+// @CreateAt 1/15/2022 3:36 PM:00
 package sort
 
+// InsertionInt
+// @Description: 插入排序
+// @param arr
+// @param desc
+// @return []int
 func InsertionInt(arr []int, desc bool) []int {
-	for i := 1; i < len(arr); i++ {
-		key := arr[i]
-		j := i - 1
-		for j >= 0 && (!(arr[j] > key) && desc || arr[j] > key && !desc) {
-			arr[j+1] = arr[j]
-			j--
-		}
-		arr[j+1] = key
-	}
-	return arr
-}
-
-func InsertionInt8(arr []int8, desc bool) []int8 {
-	for i := 1; i < len(arr); i++ {
-		key := arr[i]
-		j := i - 1
-		for j >= 0 && (!(arr[j] > key) && desc || arr[j] > key && !desc) {
-			arr[j+1] = arr[j]
-			j--
-		}
-		arr[j+1] = key
-	}
-	return arr
-}
-
-func InsertionInt16(arr []int16, desc bool) []int16 {
-	for i := 1; i < len(arr); i++ {
-		key := arr[i]
-		j := i - 1
-		for j >= 0 && (!(arr[j] > key) && desc || arr[j] > key && !desc) {
-			arr[j+1] = arr[j]
-			j--
-		}
-		arr[j+1] = key
-	}
-	return arr
-}
-
-func InsertionInt32(arr []int32, desc bool) []int32 {
-	for i := 1; i < len(arr); i++ {
-		key := arr[i]
-		j := i - 1
-		for j >= 0 && (!(arr[j] > key) && desc || arr[j] > key && !desc) {
-			arr[j+1] = arr[j]
-			j--
-		}
-		arr[j+1] = key
-	}
-	return arr
-}
-
-func InsertionInt64(arr []int64, desc bool) []int64 {
-	for i := 1; i < len(arr); i++ {
-		key := arr[i]
-		j := i - 1
-		for j >= 0 && (!(arr[j] > key) && desc || arr[j] > key && !desc) {
-			arr[j+1] = arr[j]
-			j--
-		}
-		arr[j+1] = key
-	}
-	return arr
-}
-
-func InsertionUint(arr []uint, desc bool) []uint {
-	for i := 1; i < len(arr); i++ {
-		key := arr[i]
-		j := i - 1
-		for j >= 0 && (!(arr[j] > key) && desc || arr[j] > key && !desc) {
-			arr[j+1] = arr[j]
-			j--
-		}
-		arr[j+1] = key
-	}
-	return arr
-}
-
-func InsertionUint8(arr []uint8, desc bool) []uint8 {
-	for i := 1; i < len(arr); i++ {
-		key := arr[i]
-		j := i - 1
-		for j >= 0 && (!(arr[j] > key) && desc || arr[j] > key && !desc) {
-			arr[j+1] = arr[j]
-			j--
-		}
-		arr[j+1] = key
-	}
-	return arr
-}
-
-func InsertionUint16(arr []uint16, desc bool) []uint16 {
-	for i := 1; i < len(arr); i++ {
-		key := arr[i]
-		j := i - 1
-		for j >= 0 && (!(arr[j] > key) && desc || arr[j] > key && !desc) {
-			arr[j+1] = arr[j]
-			j--
-		}
-		arr[j+1] = key
-	}
-	return arr
-}
-
-func InsertionUint32(arr []uint32, desc bool) []uint32 {
-	for i := 1; i < len(arr); i++ {
-		key := arr[i]
-		j := i - 1
-		for j >= 0 && (!(arr[j] > key) && desc || arr[j] > key && !desc) {
-			arr[j+1] = arr[j]
-			j--
-		}
-		arr[j+1] = key
-	}
-	return arr
-}
-
-func InsertionUint64(arr []uint64, desc bool) []uint64 {
-	for i := 1; i < len(arr); i++ {
-		key := arr[i]
-		j := i - 1
-		for j >= 0 && (!(arr[j] > key) && desc || arr[j] > key && !desc) {
-			arr[j+1] = arr[j]
-			j--
-		}
-		arr[j+1] = key
-	}
-	return arr
-}
-
-func InsertionFloat32(arr []float32, desc bool) []float32 {
-	for i := 1; i < len(arr); i++ {
-		key := arr[i]
-		j := i - 1
-		for j >= 0 && (!(arr[j] > key) && desc || arr[j] > key && !desc) {
-			arr[j+1] = arr[j]
-			j--
-		}
-		arr[j+1] = key
-	}
-	return arr
-}
-
-func InsertionFloat64(arr []float64, desc bool) []float64 {
 	for i := 1; i < len(arr); i++ {
 		key := arr[i]
 		j := i - 1
