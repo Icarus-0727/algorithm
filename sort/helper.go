193e24597de3c487fa5e7cc6c947e84cbdcef752
Icarus <520icarus1314@gmail.com>
2022-01-25 10:37:47 +0800

    feat: 添加工具方法
    1. swap: 交换两个元素
    2. xor: 对两个 bool 类型的变量进行异或运算

---
+++ sort/helper.go
@@ -0,0 +1,24 @@
+// Package sort
+// @Author Icarus-0727
+// @QQ 1067177968
+// @CreateAt 2022/1/25 10:15:00
+package sort
+
+// swap
+// @Description: 交换两个指针指向的元素的值
+// @param a
+// @param b
+func swap(a, b *int) {
+	*a ^= *b
+	*b ^= *a
+	*a ^= *b
+}
+
+// xor
+// @Description: 自定义 bool 类型的异或运算
+// @param a
+// @param b
+// @return bool
+func xor(a, b bool) bool {
+	return !a && b || a && !b
+}
